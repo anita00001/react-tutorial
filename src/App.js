40719973e50ae050144ea57ffbb65d561b7c8ceb
Anita Sharma <sharma.anita00001@gmail.com>
2023-04-10 17:50:51 +0545

    Raising and handling Events

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import TodosLogic from './components/TodosLogic/TodosLogic';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <TodosLogic />
     </div>
   );
 }
---
+++ src/components/TodosLogic/TodosList/TodosList.js
@@ -0,0 +1,17 @@
+import TodoItem from "./TodoItem/TodoItem";
+
+const TodosList = ({ todosProps, handleChange }) => {
+    return (
+      <ul>
+        {todosProps.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            itemProp={todo}
+            handleChange={handleChange}
+          />
+        ))}
+      </ul>
+    );
+  };
+  export default TodosList;
+  
