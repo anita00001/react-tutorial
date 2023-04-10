7066a3db50dd566270c2da4faefaf2c78d398490
Anita Sharma <sharma.anita00001@gmail.com>
2023-04-10 17:48:55 +0545

    Design Header section

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="title">
+            <h1>Today's To-Do List</h1>
+            <i className="fa-solid fa-rotate"></i>
+        </header>
+    );
+}
+
+export default Header;
