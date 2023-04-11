33855290b2c0a4d6df2e60c7f73d9f7df91a72bb
Anita Sharma <sharma.anita00001@gmail.com>
2023-04-11 07:58:04 +0545

    Style TodoItem

--- src/components/TodosLogic/TodosList/TodoItem/TodoItem.js
+++ src/components/TodosLogic/TodosList/TodoItem/TodoItem.js
@@ -1,11 +1,19 @@
+import './TodoItem.css';
+
 const TodoItem = ({ itemProp, handleChange }) => (
-  <li>
+  <li className="task-row">
     <input
       type="checkbox"
       checked={itemProp.completed}
       onChange={() => handleChange(itemProp.id)}
     />
-    {itemProp.title}
+    <p id="task-description" contenteditable="true">{itemProp.title}</p>
+    <button
+      type="button"
+      className="trash"
+      onClick={() => removeTask(itemProp.id)}>
+        <i className="fa-solid fa-trash-can"></i>
+    </button>
   </li>
 );
 export default TodoItem;
